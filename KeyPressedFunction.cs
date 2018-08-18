70c31a766ff955d458439ed8f6a51c4ec056bd4d
tama.shima <minatodaisuki2828@gmail.com>
2018-08-18 18:35:54 +0900

    新規追加

---
+++ KeyPressedFunction.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void KeyFunction();
+
+public class KeyPressedFunction
+{
+    public enum ePressedType
+    {
+        ePressedTypePressed,
+        ePressedTypeDown,
+        ePressedTypeUp,
+    };
+
+    private KeyFunction keyFunction;
+    private ePressedType pressedType;
+
+    public KeyPressedFunction(ePressedType type, KeyFunction keyFunction)
+    {
+        this.pressedType = type;
+        this.keyFunction = keyFunction;
+    }
+
+    public void InputKey(KeyCode keyCode)
+    {
+        switch (pressedType)
+        {
+            case ePressedType.ePressedTypePressed:
+                if(Input.GetKey(keyCode))
+                {
+                    run();
+                }
+                break;
+            case ePressedType.ePressedTypeDown:
+                if (Input.GetKeyDown(keyCode))
+                {
+                    run();
+                }
+                break;
+            case ePressedType.ePressedTypeUp:
+                if (Input.GetKeyUp(keyCode))
+                {
+                    run();
+                }
+                break;
+        }
+    }
+
+    private void run()
+    {
+        this.keyFunction();
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    IDictionary<KeyCode, KeyPressedFunction> keyFuncMap;
+
+    // Use this for initialization
+    void Start () {
+        keyFuncMap = new Dictionary<KeyCode, KeyPressedFunction>();
+
+        keyFuncMap.Add(KeyCode.W, new KeyPressedFunction(KeyPressedFunction.ePressedType.ePressedTypePressed, pressedKeyW));
+        keyFuncMap.Add(KeyCode.A, new KeyPressedFunction(KeyPressedFunction.ePressedType.ePressedTypePressed, pressedKeyA));
+        keyFuncMap.Add(KeyCode.S, new KeyPressedFunction(KeyPressedFunction.ePressedType.ePressedTypePressed, pressedKeyS));
+        keyFuncMap.Add(KeyCode.D, new KeyPressedFunction(KeyPressedFunction.ePressedType.ePressedTypePressed, pressedKeyD));
+        keyFuncMap.Add(KeyCode.Space, new KeyPressedFunction(KeyPressedFunction.ePressedType.ePressedTypeDown, pressedKeySpace));
+    }
+
+    public delegate void keyFunction();
+
+    void pressedKeyW()
+    {
+        Debug.Log("keyW");
+    }
+
+    void pressedKeyA()
+    {
+        Debug.Log("keyA");
+    }
+
+    void pressedKeyS()
+    {
+        Debug.Log("keyS");
+    }
+
+    void pressedKeyD()
+    {
+        Debug.Log("keyD");
+    }
+
+    void pressedKeySpace()
+    {
+        Debug.Log("keySpace");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        KeyCode[] useKeyArray = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Space };
+
+        foreach(KeyCode keyCode in useKeyArray)
+        {
+            if (keyFuncMap.ContainsKey(keyCode))
+            {
+                keyFuncMap[keyCode].InputKey(keyCode);
+            }
+        }
+
+	}
+}
